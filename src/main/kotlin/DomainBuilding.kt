a48a64c462b3c6a776d81980e7d29d93905a59c9
soadabd <soadabd689@gmail.com>
2025-12-08 11:43:30 +0200

    Creat domain list

--- src/main/kotlin/DomainBuilding.kt
+++ src/main/kotlin/DomainBuilding.kt
@@ -1,2 +1,49 @@
+import com.bytebloom.model.raw.MenteeRaw
+import com.bytebloom.model.raw.PerformanceRaw
+import com.bytebloom.model.raw.TeamRaw
+import domain.PerformanceSubmission
+import domain.Mentee
+import domain.Team
+
 class DomainBuilding {
+    fun bildDomain(
+        PerformanceRawList : List<PerformanceRaw>,
+        teamRawList : List<TeamRaw>,
+        menteeRawList : List<MenteeRaw>){
+
+        val performanceSubmission = PerformanceRawList.map {
+            performanceSubmissionRaw ->
+            PerformanceSubmission(
+                menteeId = performanceSubmissionRaw.menteeId,
+                submissionId = performanceSubmissionRaw.submissionId,
+                submissionType = performanceSubmissionRaw.submissionType,
+                score = performanceSubmissionRaw.score
+            )
+
+        }
+
+        val teams = teamRawList.map {
+            teamRaw ->
+            Team(
+                menteeId = teamRaw.menteeId,
+                submissionId = teamRaw.submissionId,
+                mentorLead = teamRaw.mentorLead,
+                mentees = null
+            )
+
+        }
+
+        val mentee = menteeRawList.map { menteeRaw ->
+            Mentee(
+                menteeId = menteeRaw.Id,
+                name = menteeRaw.name,
+                teamId = menteeRaw.teamId,
+                submissions = null
+            )
+
+        }
+
+
+
+    }
 }
