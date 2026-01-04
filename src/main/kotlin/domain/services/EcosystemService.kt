94d9c6f687dcf99d7bb8ebe02407d3e2ebe15dfa
yara-jamal <yara421201443@gmail.com>
2026-01-04 15:19:44 +0300

    added EcosystemService class inside the service package located within the domain package.

--- src/main/kotlin/domain/services/EcosystemService.kt
+++ src/main/kotlin/domain/services/EcosystemService.kt
@@ -1,6 +1,13 @@
-package domain
+package domain.services
 
-import repositories.*
+import domain.model.Mentee
+import domain.model.Team
+import domain.repository.contracts.AttendanceRepository
+import domain.repository.contracts.MenteeRepository
+import domain.repository.contracts.PerformanceSubmissionRepository
+import domain.repository.contracts.ProjectRepository
+import domain.repository.contracts.TeamRepository
+
 class EcosystemService(
     private val teamRepository: TeamRepository,
     private val menteeRepository: MenteeRepository,
