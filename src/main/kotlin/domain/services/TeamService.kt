b01bb576e2e253c83aabe94d1782e1903b5f40a5
ALBatool8 <tot28820041@gmail.com>
2026-01-08 15:34:18 +0200

    implement TeamService with mentor search and performance calculation

--- src/main/kotlin/domain/services/TeamService.kt
+++ src/main/kotlin/domain/services/TeamService.kt
@@ -1,4 +1,27 @@
 package domain.services
 
-class TeamService {
+import domain.model.Team
+import domain.repository.contracts.PerformanceSubmissionRepository
+import domain.repository.contracts.TeamRepository
+
+class TeamService(
+    private val teamRepository: TeamRepository,
+    private val performanceRepository: PerformanceSubmissionRepository
+) {
+
+    fun findLeadMentorForMentee(menteeId: String): String? {
+        val allTeams = teamRepository.getAllTeams()
+        val team = allTeams.find { team ->
+            team.members.any { it.id == menteeId }
+        }
+        return team?.mentorLead
+    }
+    fun getOverallPerformanceAverageForTeam(teamId: String): Double {
+        val allSubmissions = performanceRepository.getAllPerformanceSubmission()
+        val teamSubmissions = allSubmissions.filter { it.id == teamId }
+        if (teamSubmissions.isEmpty()) return 0.0
+        val totalScore = teamSubmissions.sumOf { it.score.toDouble()}
+
+        return totalScore / teamSubmissions.size
+    }
 }
