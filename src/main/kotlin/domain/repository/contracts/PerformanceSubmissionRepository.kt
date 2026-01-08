d7b42e55f4ac08cfe5f908abe99215ebc3f8b768
ALBatool8 <tot28820041@gmail.com>
2026-01-08 15:38:40 +0200

    implement mentee performance logic and expand repository contracts

--- src/main/kotlin/domain/repository/contracts/PerformanceSubmissionRepository.kt
+++ src/main/kotlin/domain/repository/contracts/PerformanceSubmissionRepository.kt
@@ -1,7 +1,10 @@
 package domain.repository.contracts
 
+import domain.model.Mentee
 import domain.model.PerformanceSubmission
 
 interface PerformanceSubmissionRepository {
     fun getAllPerformanceSubmission(): List<PerformanceSubmission>
+    fun getOverallPerformanceAverageForTeam(teamId: String): Double
+    fun findTopScoringMenteeOverall(): Mentee?
 }
